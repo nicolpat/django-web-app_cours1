31898d6ebde05fee3721e51bb1d35dc95faf5108
Nicolas Le Paih <nicolas.le.paih56@gmail.com>
2024-02-05 11:21:49 +0100

    Sauvegardez des données dans une base de données avec un modèle et une migration

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+class Title(models.Model):
+    name = models.fields.CharField(max_length=100)
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,15 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listings.models import Band, Title
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+    """)
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 
 def listings(request):
-    return HttpResponse('<h1>List</h1> <p></p>')
+    titles = Title.objects.all()
+    return HttpResponse(f"""
+            <h1>Liste !</h1>
+            <p>Les evenements à venir sont :<p>
+            <ul>
+                <li>{titles[0].name}</li>
+                <li>{titles[1].name}</li>
+                <li>{titles[2].name}</li>
+            </ul>
+    """)
 
 def contact(request):
     return HttpResponse('<h1>Contact</h1> <p></p>')
