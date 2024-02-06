3ef0465af42739636ca82e93dad3f91adb9b4e56
Nicolas Le Paih <nicolas.le.paih56@gmail.com>
2024-02-06 16:29:13 +0100

    Add banner, href between list and contact form

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+
+admin.site.register(Band)
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -21,8 +21,11 @@ from listings import views
 
 urlpatterns = [
 path('admin/', admin.site.urls),
-path('hello/', views.hello),
-path('about-us/', views.about),
-path('listings/', views.listings),
-path('contact-us/', views.contact),
+path('bands/', views.band_list, name='band-list'),
+path('bands/<int:id>/', views.band_detail, name='band-detail'),
+path('about-us/', views.about, name='about-us'),
+path('listings/', views.listing_list, name='listing-list'),
+path('listings/<int:id>/', views.listing_detail, name='listing-detail'),
+path('contact-us/', views.contact, name='contact-us'),
+path('confirmation/', views.email_sent, name='email-sent'),
 ]
